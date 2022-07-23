1f61838e89ffb9e291eb6f1d20972ae185bea9ae
Evan Alif Widhyatma <56740250+evanalif113@users.noreply.github.com>
2022-07-23 18:20:41 +0700

    Menambahkan Thingspeak

--- ESP32 Firebase RTDB.ino
+++ ESP32 Firebase RTDB.ino
@@ -1,19 +1,22 @@
-#include <Arduino.h>
 #include <WiFi.h>
+#include <WiFiClient.h>
 #include <Firebase_ESP_Client.h>
 //#include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 #include "time.h"
+#include "ThingSpeak.h"
 
-#define API_KEY       "AIzaSyCnqizfZOBY3_s-JcZnSZ9Q0Xn5GdBEB8Q"
-#define USER_EMAIL    "evanalifwidhyatma@gmail.com"
-#define USER_PASSWORD "210703"
-#define DATABASE_URL  "https://jerukagung-meteorologi-default-rtdb.asia-southeast1.firebasedatabase.app/"
+#define API_KEY       "YOUR API KEY"
+#define USER_EMAIL    "YOUR AUTHENTICATION EMAIL"
+#define USER_PASSWORD "YOUR AUTHENTICATION PASSWORD"
+#define DATABASE_URL  "YOUR DATABASE URL/"
 
-const char* ssid     = "Jerukagung Seismologi";   // your network SSID (name)
-const char* password = "seiscalogi";   // your network password
+const char* ssid      = "Jerukagung Seismologi";   // your network SSID (name)
+const char* password  = "seisca";   // your network password
 const char* ntpServer = "time.google.com";
+
+WiFiClient client;
 
 //Firebase Lamp Indicator
 const int ledStat = 2;
@@ -31,14 +34,18 @@ String uid;
 // Database main path (to be updated in setup with the user UID)
 String databasePath;
 // Database child nodes
-String tempPath = "/temp";
+String tempPath  = "/temp";
 //String humPath = "/humidity";
-String presPath = "/press";
-String timePath = "/timestamp";
+String presPath  = "/press";
+String dewPath   = "/dew";
+String hidePath  = "/hide";
+String timePath  = "/timestamp";
+String heapPath  = "/espheapram";
 
 // Parent Node (to be updated in every loop)
 String parentPath;
 int timestamp;
+int espheapram;
 FirebaseJson json;
 
 // BMP280 sensor
@@ -49,7 +56,7 @@ float pressure;
 
 // Timer variables (send new readings every three minutes)
 unsigned long CountMillis = 0;
-unsigned long timerDelay = 15000;
+unsigned long FirebaseDelay = 15000;
 
 // Initialize BMP280
 void initBMP() {
@@ -88,7 +95,8 @@ void setup() {
   pinMode(ledStat,OUTPUT);
   initBMP();
   initWiFi();
-  configTime(0, 0, ntpServer);
+  ThingSpeak.begin(client);
+  configTime(0, 0, ntpServer); //NTP Server
   // Assign the api key (required)
   config.api_key = API_KEY;
   // Assign the user sign in credentials
@@ -111,8 +119,7 @@ void setup() {
   }
   // Print user UID
   uid = auth.token.uid.c_str();
-  Serial.print("User UID: ");
-  Serial.println(uid);
+  Serial.print("User UID: "); Serial.println(uid);
 
   // Update database path
   databasePath = "/data/meteorologi";
@@ -120,24 +127,71 @@ void setup() {
 
 void FirebaseSend() {
   // Send new readings to database
-  if (Firebase.ready() && (millis() - CountMillis > timerDelay || CountMillis == 0)) {
+  if (Firebase.ready() && (millis() - CountMillis > FirebaseDelay || CountMillis == 0)) {
     digitalWrite(ledStat,HIGH);
     timestamp = getTime();
-    Serial.print ("Time: ");
-    Serial.println (timestamp);
+    espheapram = ESP.getFreeHeap();
+    Serial.print ("Time: ");    Serial.println (timestamp);
+    Serial.print ("HeapRAM: "); Serial.println (espheapram);
 
     parentPath = databasePath + "/" + String(timestamp);
 
-    json.set(tempPath.c_str(), String(bmp.readTemperature()));
+    json.set(tempPath.c_str(),  String(bmp.readTemperature()));
     //json.set(humPath.c_str(), String(bme.readHumidity()));
-    json.set(presPath.c_str(), String(bmp.readPressure() / 100.0F));
-    json.set(timePath, String(timestamp));
+    json.set(presPath.c_str(),  String(bmp.readPressure() / 100.0F));
+    json.set(heapPath,          String(espheapram));
+    json.set(timePath,          String(timestamp));
 
     Serial.printf("Set json... %s\n", Firebase.RTDB.setJSON(&fbdo, parentPath.c_str(), &json) ? "ok" : fbdo.errorReason().c_str());
     CountMillis = millis();
     digitalWrite(ledStat,LOW);
+    Serial.println(ESP.getFreeHeap());
+  }
+}
+
+// Timer variables
+unsigned int myChannelNumber = YOUR NUMBER CHANNEL ;
+const char* myWriteAPIKey = "YOUR WRITE API KEY";
+unsigned long lastTime = 0;
+unsigned long thingspeakDelay = 59000;
+
+void Thingspeak() {
+  if ((millis() - lastTime) > thingspeakDelay) {
+    // Get a new Total reading
+    float temperatureTot = bmp.readTemperature();
+    Serial.print("Temperature (ºC): ");
+    Serial.println(temperatureTot);
+    /*humidityTot = htu.readHumidity();
+      Serial.print("Humidity (%): ");
+      Serial.println(humidityTot);*/
+    float pressureTot = bmp.readPressure() / 100.0F;
+    Serial.print("Pressure (hPa): ");
+    Serial.println(pressureTot);
+
+    // set the fields with the values
+    ThingSpeak.setField(1, temperatureTot);
+    //ThingSpeak.setField(2, humidityTot);
+    ThingSpeak.setField(3, pressureTot);
+
+    //HTTP Kode untuk mengecek berhasil atau gagal
+    int http = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+               ThingSpeak.setStatus(String(http));
+
+    if (http == 200) {
+      //digitalWrite(ledGood, HIGH);
+      //digitalWrite(ledFail, LOW);
+      Serial.println("Data Berhasil Update. Kode HTTP" + String(http));
+    }
+    else {
+      //digitalWrite(ledFail, HIGH);
+      //digitalWrite(ledGood, LOW);
+      Serial.println("Data Gagal Update. Kode HTTP" + String(http));
+    }
+    lastTime = millis();
+    Serial.println(ESP.getFreeHeap());
   }
 }
 void loop() {
   FirebaseSend();
+  Thingspeak();
 }
